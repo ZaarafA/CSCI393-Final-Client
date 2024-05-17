630d8db1478184537e411a9a24f04f560464983d
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-05-17 14:23:00 -0400

    New Student Show Error Message

--- src/components/views/NewStudentView.js
+++ src/components/views/NewStudentView.js
@@ -35,7 +35,7 @@ const useStyles = makeStyles( () => ({
 }));
 
 const NewStudentView = (props) => {
-  const {handleChange, handleSubmit } = props;
+  const {handleChange, handleSubmit, errors } = props;
   const classes = useStyles();
 
   // Render a New Student view with an input form
@@ -88,6 +88,9 @@ const NewStudentView = (props) => {
             <br/>
           </form>
           </div>
+          {errors.firstname && <p>{errors.firstname}</p>}
+          {errors.lastname && <p>{errors.lastname}</p>}
+          {errors.email && <p>{errors.email}</p>}
       </div>
     </div>    
   )
