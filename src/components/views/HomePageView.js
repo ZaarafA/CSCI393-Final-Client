ddacde70bd2aff3ac40d2e45a664a8ffc6d8e4dc
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-05-16 22:41:43 -0400

    Home Page Styling

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -7,8 +7,19 @@ It constructs a React component to display the home page.
 const HomePageView = () => {
   // Render Home page view
   return (
-    <div >
+    <div>
       <h1>Home Page</h1>
+      <hr></hr>
+      <div  className="home-page">
+        <a href="/campuses" className="home-campuses">
+          <img src="https://picsum.photos/id/49/600/400" className="home-image"></img>
+          <div className="home-image-title"><h3>All Campuses</h3></div>
+        </a> 
+        <a href="/students" className="home-students">
+          <img src="https://picsum.photos/id/180/600/400" className="home-image"></img>
+          <div className="home-image-title"><h3>All Students</h3></div>
+        </a>
+      </div>
     </div>
   );    
 }
